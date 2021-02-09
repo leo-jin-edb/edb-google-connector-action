7fe5bc1c73ee0a0e4916b9c9d93a9ce4ea351bb7
leo.jin <leo.jin@enterprisedb.com>
2021-02-09 09:49:21 -0500

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const goog = require('./src/index')
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -11,6 +12,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+  goog.execute()
 } catch (error) {
   core.setFailed(error.message);
 }
