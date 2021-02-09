83690c03500b121530cb687e5622e637d009e60a
leo.jin <leo.jin@enterprisedb.com>
2021-02-09 09:53:23 -0500

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const { writeData } = require('./google-sheet')
 function execute() {
   const sa_email = process.env.GOOG_SA_EMAIL
   const sa_key = process.env.GOOG_SA_KEY
+  console.log(`connecting to google with sa email = ${sa_email} and sa_key = ${sa_key}`)
   const scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/calendar']
   const jwtClient = new google.auth.JWT(sa_email, null, sa_key, scope)
   jwtClient.authorize(function (err, tokens) {
