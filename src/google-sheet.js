3bd73dca7b211760293d3334e34797e5fd6dc129
leo.jin <leo.jin@enterprisedb.com>
2021-02-09 09:38:00 -0500

    using service acct

--- src/google-sheet.js
+++ src/google-sheet.js
@@ -1,17 +1,16 @@
 const { google } = require('googleapis')
 
 const writeData = async (auth) => {
-    try {
-        console.log('write data called')
-        const drive = google.drive({ version: 'v3', auth })
-        const sheets = google.sheets({ version: 'v4', auth })
-        const folders = await drive.files.list({}, auth)
-        console.log('folders = ', folders)
-    } catch (e) {
-        console.log('error = ', e)
-    }
+  try {
+    const drive = google.drive({ version: 'v3', auth })
+    const sheets = google.sheets({ version: 'v4', auth })
+    const folders = await drive.files.list()
+    console.log('folders = ', folders.data)
+  } catch (e) {
+    console.log('error = ', e)
+  }
 }
 
 module.exports = {
-    writeData
+  writeData,
 }
