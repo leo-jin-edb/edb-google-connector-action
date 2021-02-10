1e5335f1cae29cb86a4b4d07f155d46ff23a07d7
leo.jin <leo.jin@enterprisedb.com>
2021-02-10 15:47:44 -0500

    update

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const github = require('@actions/github')
 const goog = require('./src/index')
 
 try {
-  const metrics = core.getInput('metrics')
-  const { ticketKey } = metrics
-  if (ticketKey && ticketKey !== '') {
-    core.info(`Found ticket info, we will sent data tickey key = '${ticketKey}'`)
-    goog.execute(metrics)
+  let metrics = core.getInput('metrics')
+  if (metrics) {
+    core.info(`Metrics found metrics = ${metrics}`)
+    metrics = JSON.parse(metrics)
+    const { ticketKey } = metrics
+    if (ticketKey && ticketKey !== '') {
+      core.info(`Found ticket info, we will sent data tickey key = '${ticketKey}'`)
+      goog.execute(metrics)
+    } else {
+      core.info(`No ticket key found, skipping...`)
+    }
   } else {
-    core.info(`No ticket key found, skipping...`)
+    core.warning('No metrics found, skipping....')
   }
 } catch (error) {
   core.setFailed(error.message)
